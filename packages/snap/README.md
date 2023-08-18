f757f8bf630443e110ac7f0410a7fb2a46e5ed2a
Zhuo Chen <472805416@qq.com>
2023-08-18 14:56:25 +0800

    v1.0

---
+++ packages/snap/README.md
@@ -0,0 +1,37 @@
+# Mopsus Snap
+
+Mopsus Snap utilizes *pre-execution* technology to predict the users' balance changes of transactions and provide security checks before signing transactions.
+
+## Application Scenario
+Web3 users leverage wallets to sign transactions to join the DeFi ecosystem, however, most users lack the understanding of transaction content and impact.
+This ignorance causes users to be deceived by phishers, mistakenly sign malicious transactions, and cause losses to their own assets.
+
+## Snap Features
+
+### Balance Change Check
+
+Mopsus Snap integrates BlockSec Mopsus API, realizes transaction pre-running, and predicts the impact of transactions on user account assets (ERC-20 tokens). 
+
+
+### Security Check
+
+Mopsus Snap provides the ability to check the security of transactions, thereby preventing most phishing, scams, and other behaviors that endanger users. The snap integrates the address tags of the BlockSec AML team, including Fake_Phishing addresses and scam token addresses. Specifically, the snap provides the following security check:
+- Address security check
+- Token security check 
+
+# Use case
+
+## Risk Address Warning
+Sending ETH to 0xe15e7a2dda262e7b4f497f7e74e0aae5f8bd87f1 (Fake_Phishing) will be warned by dangerous address detection.
+## Scam Token Warning
+Swap USDT to 0xe3D53306be55EFF65E6A9C6fF40552c467f85482 (Fake_Phishing186264) will be warned by dangerous token detection.
+
+## License
+
+Snap is MIT licensed.
+
+
+## About US
+[BlockSec](https://blocksec.com/#about) is dedicated to building blockchain security infrastructure. The team is founded by top-notch security researchers and experienced experts from both academia and industry.
+We have published multiple blockchain security papers in prestigious conferences, reported several zero-day attacks of DeFi applications, and successfully protected digital assets that are worth more than 5 million dollars by blocking multiple attacks.
+
---
+++ packages/snap/src/index.ts
@@ -0,0 +1,318 @@
+import {
+  OnRpcRequestHandler,
+  OnTransactionHandler,
+  OnTransactionResponse
+} from '@metamask/snap-types';
+import { copyable, divider, heading, panel, text } from '@metamask/snaps-ui';
+
+/**
+ * Handle incoming Transaction requests.
+ *
+ * @param args - The request handler args as object.
+ * @param args.transaction - The  transaction object of the request, from metamask.
+ * @param args.chainId - The chainId of the request.
+ * @returns insights if the request succeeded.
+ * @throws If the `simulationTx` call failed.
+ */
+export const onTransaction: OnTransactionHandler = async ({
+  transaction,
+  chainId,
+}) => {
+  try {
+    console.log(transaction, chainId)
+    const chain_ID = chainId.includes(':') ? roughScale(chainId.split(':')[1], 16) : roughScale(chainId, 16);
+    if (chain_ID != 1 && chain_ID != 56) {
+      const insights = { 'Mopsus Warning': ' Mopsus does not support this chain yet.' }
+      return { insights }
+    } else {
+      const retObj = await simulationTx(transaction, chainId)
+      // console.log(retObj)
+      const response = await DashBoard(retObj)
+      return response;
+    }
+  } catch (e) {
+    console.log(e)
+    throw e
+  }
+};
+
+
+export interface Assets {
+  token: string
+  amount: number
+  asvalue: number
+  decimals: number
+}
+
+export interface Bundle {
+  in: Assets[]
+  out: Assets[]
+  total: number
+}
+
+export interface Sec {
+  reciver: string[]
+  token: string[]
+  approve: number
+}
+
+export interface Ret {
+  code: number
+  bundle: Bundle
+  security: Sec
+  msg?: string
+}
+
+const roughScale = (x: string, base: number) => {
+  const parsed = parseInt(x, base);
+  if (isNaN(parsed)) { return 0; }
+  return parsed;
+}
+
+/**
+ * Transaction Security Check API requests.
+ *
+ * @param args - The request handler args as object.
+ * @param args.transaction - The  transaction object of the request, from metamask.
+ * @param args.chainId - The chainId of the request.
+ * @returns Retuen Json Object if the request succeeded.
+ * @throws If the CORS HTTPs request call failed.
+ */
+export const simulationTx = async (transaction: any, chainId: string) => {
+  const TX_API_ENDPOINT =
+    'https://flask.blocksec.com/TxCheck';
+  // POST body
+  const body = {
+    richMan: true,
+    chainID: chainId.includes(':') ? roughScale(chainId.split(':')[1], 16) : roughScale(chainId, 16),
+    bundle: [
+      {
+        sender: transaction.from.toLowerCase(),
+        receiver: transaction.to.toLowerCase(),
+        value: roughScale(transaction.value, 16).toString(),
+        input: transaction.data ?
+          transaction.data.toLowerCase() : "0x",
+        gasLimit: transaction.gas ?
+          roughScale(transaction.gas, 16) : 28500000,
+        gasPrice: transaction.gasPrice ?
+          roughScale(transaction.gasPrice, 16).toString() : "",
+        maxFeePerGas: transaction.maxFeePerGas ?
+          roughScale(transaction.maxFeePerGas, 16).toString() : "",
+        maxPriorityFeePerGas: transaction.maxPriorityFeePerGas ?
+          roughScale(transaction.maxPriorityFeePerGas, 16).toString() : "",
+      },
+    ],
+  };
+
+  console.log(`metamsk data${JSON.stringify(transaction)}`)
+  console.log(`raw data${JSON.stringify(body)}`);
+
+  // Only allow Content-Type, X-CSRF-Token, Authorization, AccessToken, Token in MetaMask Flask
+  const res = await fetch(TX_API_ENDPOINT, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    mode: 'cors',
+    body: JSON.stringify(body),
+  });
+  if (!res.ok) {
+    throw new Error('Bad response from server');
+  }
+  const json = await res.json();
+  console.log(`return data${JSON.stringify(json)}`);
+  const retObj: Ret = JSON.parse(JSON.stringify(json));
+  return retObj;
+};
+
+export type DashBoardHandler = (retObj: Ret) => Promise<OnTransactionResponse>;
+
+
+const splitThousandSeparator = (num: number, dec: number, unit: string) => {
+  if (Math.abs(num) < 1e-8) {
+    return unit + "0"
+  }
+  let head: string = "";
+  if (num < 0) {
+    num *= -1;
+    head = "-";
+  } else {
+    head = "+";
+  }
+  let DIGIT_PATTERN = /(^|\s)\d+(?=\.?\d*($|\s))/g;
+  let MILI_PATTERN = /(?=(?!\b)(\d{3})+\.?\b)/g;
+  let str: string = num.toFixed(dec).replace(DIGIT_PATTERN, (m) => m.replace(MILI_PATTERN, ','));
+  return " " + head + " " + unit + str
+}
+
+//MetaMask flask only accept OnTransactionResponse{[key:string]:string} input, We  
+//try our best to imporve the dashboard using '' to replace '\r'.
+/**
+ * Transaction Security Check API requests.
+ *
+ * @param args - The request handler args as object.
+ * @param args.retObj - The Json Object from API.
+ * @returns Retuen OnTransactionResponse Object.
+ */
+const DashBoard: DashBoardHandler = async (
+  retObj,
+) => {
+  let inValue = 0
+
+  // return {
+  //   content: panel([
+  //     heading('Transaction insights snap'),
+  //     text(
+  //       `1`,
+  //     ),
+  //   ]),
+  // };
+  //Simulation Failed/Revert
+  if (retObj.code != 0) {
+    const insights = {
+      content: panel([
+        heading('Mopsus Warning'),
+        text(
+          `Mopsus shows the transaction will fail.`,
+        ),
+      ]),
+    };
+    console.log(`Error at ${retObj.msg}`)
+    return  insights 
+  }
+  // const response = { insights: {} } as OnTransactionResponse
+  const dialog : any[] = [];
+  let map: string = ''
+
+  if (retObj.bundle.in == null && retObj.bundle.out == null && retObj.bundle.total == 0) {
+    // // response.insights[map] = 'No Balance Change'
+    dialog.push(heading(map))
+    dialog.push(text("No Balance Change"))
+    map += ' '
+  } else {
+    // // response.insights[`Balance Changes of Sender: ${splitThousandSeparator(retObj.bundle.total, 3, "$")}`] = ``
+    dialog.push(heading(`Sender Balance Changes: ${splitThousandSeparator(retObj.bundle.total, 3, "$")}`))
+  }
+
+  dialog.push(heading(map))
+  dialog.push(heading(map))
+
+  if (retObj.bundle.out != null) {
+    for (let i of retObj.bundle.out) {
+      if (i.asvalue != 0) {
+        const note = `${splitThousandSeparator(i.amount / Math.pow(10, i.decimals), 10, "")} ${i.token} (${splitThousandSeparator(i.asvalue, 2, "$")})`
+        const notes = note.toString()
+        console.log(note.toString())
+        // response.insights[map] = note
+        // dialog.push(heading(map))
+        dialog.push(copyable(`${splitThousandSeparator(i.amount / Math.pow(10, i.decimals), 10, "")} ${i.token} (${splitThousandSeparator(i.asvalue, 2, "$")})`))
+        map += ' '
+      } else {
+        //asvalue is 0 (valueless token or no liqudity)
+        const note = `${splitThousandSeparator(i.amount / Math.pow(10, i.decimals), 10, "")} ${i.token} (${splitThousandSeparator(i.asvalue, 2, "$")})`
+        // response.insights[map] = note
+        // dialog.push(heading(map))
+        dialog.push(copyable(note))
+        map += ' '
+      }
+    }
+  }
+  if (retObj.bundle.in != null) {
+    for (let i of retObj.bundle.in) {
+      if (i.asvalue != 0) {
+        const note = `${splitThousandSeparator(i.amount / Math.pow(10, i.decimals), 10, "")} ${i.token} (${splitThousandSeparator(i.asvalue, 2, "$")})`
+        // response.insights[map] = note
+        dialog.push(heading(map))
+        dialog.push(copyable(note))
+        map += ' '
+        inValue += i.asvalue
+      } else {
+        //asvalue is 0 (valueless token or no liqudity)
+        const note = `${splitThousandSeparator(i.amount / Math.pow(10, i.decimals), 10, "")} ${i.token} (${splitThousandSeparator(i.asvalue, 2, "$")})`
+        // response.insights[map] = note
+        dialog.push(heading(map))
+        dialog.push(copyable(note))
+        map += ' '
+        inValue += i.asvalue
+      }
+    }
+  }
+  // response.insights[map] = ''
+  dialog.push(heading(map))
+  // dialog.push(text(' '))
+  map += ' '
+  // response.insights[map] = ''
+  dialog.push(heading(map))
+  // dialog.push(text(' '))
+  map += ' '
+
+
+  // security
+  dialog.push(divider())
+
+  if ((retObj.security.token == null) &&
+    (retObj.security.reciver == null)
+  ) {
+    // response.insights['Security Check: Passed'] = ''
+    dialog.push(heading('Security Check: Passed'))
+    // dialog.push(text(' '))
+    // response.insights[map] = ''
+    dialog.push(heading(map))
+    // dialog.push(text(' '))
+    map += ' '
+  } else {
+    // response.insights['Security Check: Failed'] = ''
+    dialog.push(heading('Security Check: Failed'))
+    // dialog.push(text(' '))
+    // response.insights[map] = 'Be cautious! The transaction may cause you to lose money.'
+    dialog.push(heading(map))
+    dialog.push(text('Be cautious! The transaction may cause you to lose money.'))
+    map += ' '
+  }
+
+  //Security - Reciver
+  if (retObj.security.reciver != null) {
+    // response.insights[map] = " ✕ Risky Address Detection"
+    dialog.push(heading(map))
+    dialog.push(text('✕ Risky Address Detection'))
+    map += ' '
+    for (let i of retObj.security.reciver) {
+      //str += ` ${i}`
+      // response.insights[map] = `-- ${i}`
+      dialog.push(heading(map))
+      dialog.push(copyable(`-- ${i}`))
+      map += ' '
+    }
+  } else {
+    // response.insights[map] = ' ✓ Risky Address Detection'
+    dialog.push(heading(map))
+    dialog.push(text(' ✓ Risky Address Detection'))
+    map += ' '
+  }
+
+  //Security - Token
+  if (retObj.security.token != null) {
+    // let str: string = " ✕ Scam Token Detection"
+    // response.insights[map] = str
+    dialog.push(heading(map))
+    dialog.push(text(" ✕ Scam Token Detection"))
+    map += ' '
+    for (let i of retObj.security.token) {
+      // response.insights[map] = `-- ${i}`
+      dialog.push(heading(map))
+      dialog.push(copyable(`-- ${i}`))
+      map += ' '
+    }
+  } else {
+    // response.insights[map] = ' ✓ Scam Token Detection'
+    dialog.push(heading(map))
+    dialog.push(text(' ✓ Scam Token Detection'))
+    map += ' '
+  }
+
+  const insights = {
+    content: panel(dialog),
+  };
+  console.log(dialog)
+  return insights 
+}
